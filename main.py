9ee706219e63f8a9ad4941eacb335da8e26564ee
m-kuz <fine.arteom@yandex.ru>
2021-02-11 17:45:56 +0300

    То, что я сделал на занятии

--- main.py
+++ main.py
@@ -1,13 +1,33 @@
 import sys
-# import requests
-from PyQt5 import uic
+import requests
+
+from design import Ui_MainWindow
 from PyQt5.QtWidgets import QApplication, QMainWindow
 
 
-class Window(QMainWindow):
+class Window(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
-        uic.loadUi('design.ui', self)
+        self.setupUi(self)
+        self.coordinates = ''
+        self.scale = ''
+        self.map_file = "map.png"
+        self.get_coordinates()
+        self.get_scale()
+        self.get_image()
+
+    def get_coordinates(self):
+        self.coordinates = self.lineEdit.text()
+
+    def get_scale(self):
+        self.scale = self.lineEdit_2.text()
+
+    def get_image(self):
+        map_request = f"https://static-maps.yandex.ru/1.x/?ll={self.coordinates}&l=map&apikey=40d1649f-0493-4b70-98ba" \
+                      f"-98533de7710b&z={self.scale}"
+        response = requests.get(map_request)
+        with open(self.map_file, "wb") as f:
+            f.write(response.content)
 
 
 if __name__ == '__main__':
