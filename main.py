9ef1391762e53978c5fc7fe9f2ce86fcfc4b2ce0
Андрей Квасов <kvasan2104@gmail.com>
2021-02-13 13:58:00 +0300

    2 задача

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import sys
 import requests
 from PyQt5 import uic
-
+from PyQt5.QtCore import Qt
 # from design import Ui_MainWindow
 from PyQt5.QtGui import QPixmap
 from PyQt5.QtWidgets import QApplication, QMainWindow
@@ -14,7 +14,7 @@ class Window(QMainWindow):  # Ui_MainWindow
         # self.setupUi(self)
         self.api_key = '40d1649f-0493-4b70-98ba-98533de7710b'
         self.coordinates = '37.620070,55.753630'
-        self.scale = '10'
+        self.scale = 10
         self.size = '650,450'  # ограничение по размеру: 650х450
         self.map_file = "map.png"
         self.pixmap = QPixmap()
@@ -23,7 +23,7 @@ class Window(QMainWindow):  # Ui_MainWindow
 
     def get_image(self):
         map_request = f"https://static-maps.yandex.ru/1.x/?ll={self.coordinates}&l=map&" \
-                      f"apikey=40d1649f-0493-4b70-98ba-98533de7710b&size={self.size}&z={self.scale}"
+                      f"apikey=40d1649f-0493-4b70-98ba-98533de7710b&size={self.size}&z={str(self.scale)}"
         response = requests.get(map_request)
         with open(self.map_file, "wb") as f:
             f.write(response.content)
@@ -32,6 +32,16 @@ class Window(QMainWindow):  # Ui_MainWindow
     def set_image(self):
         self.pixmap = QPixmap(self.map_file)
         self.label.setPixmap(self.pixmap)
+
+    def keyPressEvent(self, event):
+        if event.key() == Qt.Key_PageDown:
+            if self.scale >= 1:
+                self.scale -= 1
+        if event.key() == Qt.Key_PageUp:
+            if self.scale < 17:
+                self.scale += 1
+        self.get_image()
+        self.set_image()
 
 
 def except_hook(cls, exception, traceback):
